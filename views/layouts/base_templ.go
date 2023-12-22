4b6d1614ed8eb0779bea1d10a9e81c8bed80fa93
Luciano Correia <luciano.correia@gmail.com>
2023-12-22 12:53:18 +0400

    update favicon

--- views/layouts/base_templ.go
+++ views/layouts/base_templ.go
@@ -40,7 +40,7 @@ func Base(title string) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("\"><title>")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("\"><link rel=\"icon\" type=\"image/png\" href=\"/static/favicon.png\"><title>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
